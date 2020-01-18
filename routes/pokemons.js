07c66051b18a4274290fc69051627a5e5092b30d
William Velazquez <william.velazquez.a@gmail.com>
2020-01-17 21:20:32 -0600

    Creating Service for Getting PokemonByID/PokemonByName Information

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -35,6 +35,20 @@ function pokemonAPI(app) {
     }
   });
 
+  router.get('/info/:id', async function(request, response, next) {
+    try {
+      const pokemon = await P.getPokemonByName(request.params.id);
+      const url = pokemon.species.url;
+      const info = await P.resource(url);
+      response.status(200).json({
+        data: info,
+        message: 'pokemon info listed',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
   router.get('/characteristic/:id', async function(request, response, next) {
     try {
       const characteristic = await P.getCharacteristicById(request.params.id);
@@ -46,6 +60,19 @@ function pokemonAPI(app) {
       next(error);
     }
   });
+
+  router.get('/data', async function(request, response, next) {
+    try {
+      const data = await P.resource(request.query.path);
+      response.status(200).json({
+        data: data,
+        message: 'data listed',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
 };
 
 module.exports = pokemonAPI;
