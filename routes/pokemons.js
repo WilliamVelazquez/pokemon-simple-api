15a05983196d404381ab2e1ba7406c1272388050
William Velazquez <william.velazquez.a@gmail.com>
2020-01-19 15:51:28 -0600

    Implementing Pokemons Service Layer on Routes and Adding PokemonByName and PokemonInfo Mock data

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,19 +1,24 @@
 const express = require('express');
-const Pokedex = require('pokedex-promise-v2');
-
+// const Pokedex = require('pokedex-promise-v2');
+const PokemonService = require('../services/pokemons');
 
 function pokemonAPI(app) {
-  const P = new Pokedex();
+  // const P = new Pokedex();
   const router = express.Router();
   app.use('/api/pokemons', router);
 
+  const pokemonService = new PokemonService();
+
   router.get('/', async function(request, response, next) {
     try {
+      const { quantity, initial } = request.query;
+
       const interval = {
-        limit: parseInt(request.query.quantity, 10) || 10,
-        offset: parseInt(request.query.initial, 10) || 1
+        limit: parseInt(quantity, 10) || 10,
+        offset: parseInt(initial, 10) || 1
       }
-      const pokemons = await P.getPokemonsList(interval);
+      // const pokemons = await P.getPokemonsList(interval);
+      const pokemons = await pokemonService.getPokemonsList(interval);
       response.status(200).json({
         data: pokemons,
         message: 'pokemons listed',
@@ -25,7 +30,10 @@ function pokemonAPI(app) {
 
   router.get('/:id', async function(request, response, next) {
     try {
-      const pokemon = await P.getPokemonByName(request.params.id);
+      const { id } = request.params;
+
+      // const pokemon = await P.getPokemonByName(id);
+      const pokemon = await pokemonService.getPokemonByName(id);
       response.status(200).json({
         data: pokemon,
         message: 'pokemon listed',
@@ -37,9 +45,13 @@ function pokemonAPI(app) {
 
   router.get('/info/:id', async function(request, response, next) {
     try {
-      const pokemon = await P.getPokemonByName(request.params.id);
+      const { id } = request.params;
+
+      // const pokemon = await P.getPokemonByName(id);
+      const pokemon = await pokemonService.getPokemonByName(id);
       const url = pokemon.species.url;
-      const info = await P.resource(url);
+      // const info = await P.resource(url);
+      const info = await pokemonService.getPokemonResource(url);
       response.status(200).json({
         data: info,
         message: 'pokemon info listed',
@@ -51,7 +63,10 @@ function pokemonAPI(app) {
   
   router.get('/characteristic/:id', async function(request, response, next) {
     try {
-      const characteristic = await P.getCharacteristicById(request.params.id);
+      const { id } = request.params;
+      
+      // const characteristic = await P.getCharacteristicById(id);
+      const characteristic = await pokemonService.getCharacteristicById(id);
       response.status(200).json({
         data: characteristic,
         message: 'characteristic listed',
@@ -60,19 +75,6 @@ function pokemonAPI(app) {
       next(error);
     }
   });
-
-  router.get('/data', async function(request, response, next) {
-    try {
-      const data = await P.resource(request.query.path);
-      response.status(200).json({
-        data: data,
-        message: 'data listed',
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
-  
 };
 
 module.exports = pokemonAPI;
