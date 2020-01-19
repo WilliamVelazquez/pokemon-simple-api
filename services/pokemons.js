4d8b3ae8c8386cd43e3fd5111d427f6c1968bfbf
William Velazquez <william.velazquez.a@gmail.com>
2020-01-19 15:37:47 -0600

    Creating Pokemons Mock Service and Creating Pokemons Service Layer

---
+++ services/pokemons.js
@@ -0,0 +1,34 @@
+const Pokedex = require('pokedex-promise-v2');
+// const { pokemonsMock, pokemonInfoMock } = require('../utils/mocks/pokemons');
+
+class PokemonService {
+	constructor (){
+		this.P = new Pokedex();
+	}
+
+	async getPokemonsList(interval) {
+		// const pokemons = await Promise.resolve(pokemonsMock);
+		const pokemons = await this.P.getPokemonsList(interval);
+		return pokemons || [];
+	}
+
+	async getPokemonByName(id) {
+		// const pokemonInfo = await Promise.resolve(pokemonInfoMock);
+		const pokemonInfo = await this.P.getPokemonByName(id);
+		return pokemonInfo || {};
+	}
+
+	async getPokemonResource(url) {
+		// const pokemonResource = await Promise.resolve(pokemonInfoMock);
+		const pokemonResource = await this.P.resource(url);
+		return pokemonResource || {};
+	}
+
+	async getCharacteristicById(id) {
+		// const pokemonResource = await Promise.resolve(pokemonInfoMock);
+		const characteristic = await this.P.resource(id);
+		return characteristic || {};
+	}
+}
+
+module.exports = PokemonService;
